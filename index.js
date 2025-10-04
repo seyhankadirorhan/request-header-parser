7907dd4eaac70f6fa10e87ea2fb2f8a76c5c3f42
Seyhan Kadir ORHAN <seyhanceng2003@gmail.com>
2025-10-04 12:05:50 +0300

    Add /api/whoami endpoint

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// 🚀 YOUR TASK: Request Header Parser API endpoint
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
